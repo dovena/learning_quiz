a564f2c7022da85e949a3d594a8f9bf524796f6a
dovena <hallohusky@gmail.com>
2023-07-18 10:48:06 +0200

    Initial Commit

---
+++ learning_quiz.py
@@ -0,0 +1,48 @@
+import json
+import random
+
+
+class Quiz:
+    def __init__(self, filename):
+        self.filename = filename
+        self.questions = []
+
+    def load_questions(self):
+        try:
+            with open(self.filename, "r") as file:
+                self.questions = json.load(file)
+        except FileNotFoundError:
+            self.questions = []
+
+    def run_quiz(self):
+        random.shuffle(self.questions)
+        score = 0
+
+        for i, question in enumerate(self.questions, 1):
+            print(f"Question {i}: {question['question']}")
+            for j, option in enumerate(question["options"], 1):
+                print(f"{j}. {option}")
+
+            user_answer = input("Enter your answer: ")
+            if user_answer.isdigit() and 1 <= int(user_answer) <= len(
+                question["options"]
+            ):
+                if question["options"][int(user_answer) - 1] == question["answer"]:
+                    print("Correct answer!")
+                    score += 1
+                else:
+                    print(
+                        f"Incorrect answer! The correct answer is {question['answer']}"
+                    )
+            else:
+                print("Invalid input. Skipping this question.")
+
+            print()
+
+        print(f"Quiz completed! Your score is {score}/{len(self.questions)}")
+
+
+quiz = Quiz("test_quiz.json")
+quiz.load_questions()
+
+quiz.run_quiz()
---
+++ readme.md
@@ -0,0 +1,17 @@
+# Learning Quiz
+Since I am back to school after 20 years in a completely new field, I have a hard time remembering all the content for the quizzes. This little learning quiz program helps me to learn. I write the important course content in the JSON file, which can be switched depending on the topic I have to study for.
+
+The questions and the possible answers, plus the correct answer, are written in In a JSON file. It is easy to add new questions as needed in the JSON file.
+```json
+[
+    {
+        "question": "Question Title",
+        "options": [
+            "one",
+            "two",
+            "three"
+        ],
+        "answer": "two"
+    }
+]
+```
